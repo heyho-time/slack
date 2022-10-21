2eb88a2da79e9132e48a3ed791d281b119dee3ef
성해호 <hhseong@tidesquare.com>
2022-10-21 17:41:34 +0900

    modal - stopPropagation 이벤트버블링 막아주기

---
+++ alecture/components/menu/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC, CSSProperties, useCallback } from 'react';
+import { CloseModalButton, CreateMenu } from './styles';
+
+interface Props {
+  show: boolean;
+  onCloseModal: (e: any) => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: FC<Props> = ({ children, style, show, onCloseModal, closeButton }) => {
+  const stopPropagation = useCallback((e) => {
+    e.stopPropagation();
+    // 이벤트 버블링을 막아준다.
+  }, []);
+
+  if (!show) return null;
+
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div style={style} onClick={stopPropagation}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
--- alecture/layouts/workspace/index.tsx
+++ alecture/layouts/workspace/index.tsx
@@ -1,22 +1,26 @@
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import useSWR from 'swr';
 import {
   Channels,
   Chats,
   Header,
+  LogOutButton,
   MenuScroll,
   ProfileImg,
+  ProfileModal,
   RightMenu,
   WorkspaceName,
   Workspaces,
   WorkspaceWrapper,
 } from './styles';
 import gravatar from 'gravatar';
+import Menu from '@components/menu';
 
 const Workspace: FC = ({ children }) => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const { data, error, mutate } = useSWR('/api/users', fetcher);
 
   const onLogout = useCallback(() => {
@@ -33,15 +37,30 @@ const Workspace: FC = ({ children }) => {
     return <Navigate to="/login" />;
   }
 
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu(!showUserMenu);
+  }, [showUserMenu]);
+
   return (
     <div>
       <Header>test</Header>
       <RightMenu>
-        <span>
+        <span onClick={onClickUserProfile}>
           <ProfileImg src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+          {showUserMenu && (
+            <Menu style={{ right: 0, top: 38 }} onCloseModal={onClickUserProfile} show={showUserMenu}>
+              <ProfileModal>
+                <img src={gravatar.url(data.email, { s: '28px', d: 'retro' })} alt={data.nickname} />
+                <div>
+                  <span id="profile-name">{data.nickname}</span>
+                  <span id="profile-active">Active</span>
+                </div>
+              </ProfileModal>
+              <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+            </Menu>
+          )}
         </span>
       </RightMenu>
-      <button onClick={onLogout}>로그아웃</button>
       <WorkspaceWrapper>
         <Workspaces>test</Workspaces>
         <Channels>

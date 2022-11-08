fed8b472b7c97bb44227c3d30295373135a5cb38
성해호 <hhseong@tidesquare.com>
2022-11-08 13:37:17 +0900

    리버스 인피니트 스크롤링

--- alecture/components/chatList/index.tsx
+++ alecture/components/chatList/index.tsx
@@ -1,20 +1,27 @@
 // import Chat from '@components/Chat';
 import Chat from '@components/chat';
 import { ChatZone, Section, StickyHeader } from '@components/chatList/styles';
-import { IDM, IChat } from '@typings/db';
-import React, { useCallback, forwardRef, RefObject, MutableRefObject, VFC, useRef } from 'react';
+import { IDM } from '@typings/db';
+import React, { useCallback, forwardRef } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 interface Props {
   chatSections: { [key: string]: IDM[] };
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>;
+  isEmpty: boolean;
+  isReachingEnd: boolean;
 }
-const ChatList: VFC<Props> = ({ chatSections }) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty, isReachingEnd }, ref) => {
+  const onScroll = useCallback((values: any) => {
+    if (values.scrollTop === 0 && !isReachingEnd) {
+      // console.log('top top top');
+      setSize((prevSize) => prevSize + 1).then(() => {});
+    }
+  }, []);
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date, chats]) => {
           return (
             <Section className={`section-${date}`} key={date}>
@@ -34,6 +41,6 @@ const ChatList: VFC<Props> = ({ chatSections }) => {
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;
